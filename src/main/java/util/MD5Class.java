6768f579104b551cb714826be681c4e570d9fb06
WenhaoYao <Yaowenhao.suigu@gmail.com>
2018-12-07 09:58:03 +0800

    MD5加密处理

---
+++ src/main/java/util/MD5Class.java
@@ -0,0 +1,34 @@
+package util;
+
+import java.security.MessageDigest;
+
+/**
+ * @author YaoWenHao
+ * @Title: MD5Class
+ * @ProjectName vote
+ * @Description: TODO
+ * @date 2018/12/7 9:08
+ */
+public class MD5Class {
+
+    public static String stringToMd5(String info) {
+        char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+        try {
+            byte[] strTemp = info.getBytes();
+            MessageDigest mdTemp = MessageDigest.getInstance("MD5");
+            mdTemp.update(strTemp);
+            byte[] md = mdTemp.digest();
+            int j = md.length;
+            char str[] = new char[j * 2];
+            int k = 0;
+            for (int i = 0; i < j; i++) {
+                byte byte0 = md[i];
+                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                str[k++] = hexDigits[byte0 & 0xf];
+            }
+            return new String(str);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
